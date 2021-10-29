2c75a94506161aa239923f3040c1a4fa0e4c2591
Subhashis <subhashisz@gmail.com>
2021-10-29 12:20:10 +0530

    commit

--- src/main/java/com/myapp/routs/ApiGatewayConfiguration.java
+++ src/main/java/com/myapp/routs/ApiGatewayConfiguration.java
@@ -1,4 +1,4 @@
-package com.myapp.configuration;
+package com.myapp.routs;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
